0bfb54c02d349bed29b30573b187f37dd5ad8789
khayyyyy24 <102018540+khayyyyy24@users.noreply.github.com>
2023-10-22 22:55:56 +0700

    Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-from math_function import add
+from Math_function import add, multiply, divide
 
 
 def main():
@@ -7,12 +7,21 @@ def main():
     data_2 = int(input("masukkan input 2 :"))
     operator = input("masukkan operator :")
 
+    result = None
+
     if operator == "+":
         result = add(data_1, data_2)
+    elif operator == "-":
+        result = data_1 - data_2
+    elif operator == "*":
+        result = multiply(data_1, data_2)
+    elif operator == "/":
+        if data_2 == 0:
+            print("Tidak dapat dibagi oleh nol.")
+            return
+        result = divide(data_1, data_2)
+    else:
+        print("Operator tidak valid.")
+        return
 
     print("{} {} {} = {} ".format(data_1, operator, data_2, result))
-
-
-if __name__ == "__main__":
-    print("Hello Main !")
-    main()
